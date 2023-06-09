d3258b3b6111448348f220f20d16ad3b53b24b49
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2023-06-09 02:35:04 -0300

    fix: fixed cart area showing

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -6,8 +6,11 @@ import Header from "../components/organisms/Header/Header";
 import ProductDetailsPage from "../pages/ProductDetailsPage/ProductDetailsPage";
 import CartContainer from "../components/organisms/CartContainer/CartContainer";
 import CartPage from "../pages/CartPage/CartPage";
+import { useContext } from "react";
+import { ProductsContext } from "../contexts/ProductsContext";
 
 const Router = () => {
+  const {data} = useContext(ProductsContext)
   return (
     <BrowserRouter>
       <Header />
@@ -17,7 +20,7 @@ const Router = () => {
         <Route path="/productDetails/:id" element={<ProductDetailsPage />} />
         <Route path="/cart" element={<CartPage />} />
       </Routes>
-      <CartContainer/>
+      {data?.states?.cart?.length > 0 && <CartContainer/>}
       <Footer />
     </BrowserRouter>
   );
